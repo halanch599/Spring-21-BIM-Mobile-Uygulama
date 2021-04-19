b441dfbd3f70910c4beb2406365cb286f39bbace
halanch599 <altavista5.99>
2021-04-19 16:23:39 +0300

    Lecture 9-1: Sharred Preferences
    Research about and learn about and implement it for leanring
    
    getDefaultSharedPreferences()

--- app/src/main/java/com/example/bim_1_1/MainActivity.java
+++ app/src/main/java/com/example/bim_1_1/MainActivity.java
@@ -3,24 +3,65 @@ package com.example.bim_1_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bim_1_1.model.DBUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button tbRegister;
+    Button tbRegister, btnAbout;
+    CheckBox cbRemember;
+    EditText etUserName, etPassword;
+    TextView tvMessage;
+
+    private static final String PEFERENCES_USERNAME= "username";
+    private static final String PEFERENCES_PASSWORD= "password";
+    private static final String PEFERENCES_ABOUT= "about";
+    private static final String PEFERENCES_NAME= "MY_Preference";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etUserName = (EditText) findViewById(R.id.etUserName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        tvMessage =  (TextView) findViewById(R.id.tvMessage);
         tbRegister =  findViewById(R.id.btnRegister);
+        cbRemember =  findViewById(R.id.cbRemember);
+        btnAbout = findViewById(R.id.btnAbout);
+
+//        SharedPreferences sharedPreferences = this.getPreferences(this.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = this.getSharedPreferences(PEFERENCES_NAME,this.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(PEFERENCES_ABOUT,"This is a new version of the android app. \n For testing purpose");
+
+        editor.apply(); // asynchornos save
+
+        if (sharedPreferences!=null)
+        {
+            String uname = sharedPreferences.getString(PEFERENCES_USERNAME,"");
+            String pwd = sharedPreferences.getString(PEFERENCES_PASSWORD,"");
+            etUserName.setText(uname);
+            etPassword.setText(pwd);
+            cbRemember.setChecked(true);
+            Toast.makeText(this, "Loaded from SharredPreferences", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            cbRemember.setChecked(false);
+        }
+
 
         tbRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,13 +70,21 @@ public class MainActivity extends AppCompatActivity {
                    startActivity(intent);
             }
         });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                String details = sharedPreferences.getString(PEFERENCES_ABOUT,"");
+                String details = sharedPreferences.getString(PEFERENCES_ABOUT,"");
+
+                Toast.makeText(MainActivity.this, details, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void Login(View v)
     {
-        EditText etUserName = (EditText) findViewById(R.id.etUserName);
-        EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        TextView tvMessage =  (TextView) findViewById(R.id.tvMessage);
+
 
 
         String username = etUserName.getText().toString().trim();
@@ -63,15 +112,31 @@ public class MainActivity extends AppCompatActivity {
         //if (username.equals("admin") && password.equals("12345"))
         if (DBUser.Login(username,password))
         {
-//            tvMessage.setText("Welcome to Android..");
-//            tvMessage.setVisibility(View.VISIBLE);
-//            tvMessage.setTextColor(Color.GREEN);
+            tvMessage.setText("Login Sussfully");
+            tvMessage.setVisibility(View.VISIBLE);
+            tvMessage.setTextColor(Color.GREEN);
+
+            if(cbRemember.isChecked())
+            {
+//                SharedPreferences sharedPreferences = this.getPreferences(this.MODE_PRIVATE);
+                SharedPreferences sharedPreferences = this.getSharedPreferences(PEFERENCES_NAME,this.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString(PEFERENCES_USERNAME,username);
+                editor.putString(PEFERENCES_PASSWORD,password);
+
+                editor.apply(); // asynchornos save
+//                editor.commit(); // synchornosu
+                Toast.makeText(this, "Preferrences Saved", Toast.LENGTH_SHORT).show();
+
+            }
+
 
             // Intent
 
-            Intent intent = new Intent(this,HomeActivity.class);
-            intent.putExtra("data",username);
-            startActivity(intent);
+//            Intent intent = new Intent(this,HomeActivity.class);
+//            intent.putExtra("data",username);
+//            startActivity(intent);
         }
         else{
             tvMessage.setText("Incorrect username or password");
---
+++ app/src/main/java/com/example/bim_1_1/SharedPreferrencesActivity.java
@@ -0,0 +1,14 @@
+package com.example.bim_1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SharedPreferrencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_preferrences);
+    }
+}
